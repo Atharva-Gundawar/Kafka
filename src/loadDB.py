89369694bffdf3ae51ca45c28e761d7d0ada520e
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-07-12 21:34:56 +0530

    FEAT : Source files

---
+++ src/loadDB.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+import sqlite3
+from flask import Flask
+from contextlib import closing
+
+# configuration
+DATABASE = './data.db'
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('northwind.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+if __name__ == '__main__':
+    init_db()
